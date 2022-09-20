67282f69184dcb45bd179481fa5c790a42ab1364
Noé Mouchel <noe.mouchel@gmail.com>
2022-09-20 15:05:34 +0200

    [master] lobby improvement

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
 using TMPro;
 
+// 10.2.102.127
+
 public class Lobby : MonoBehaviour
 {
     [SerializeField] private TMP_InputField m_inputClientIP;
     [SerializeField] private TMP_InputField m_inputClientPort;
     [SerializeField] private TMP_InputField m_inputHostPort;
     [SerializeField] private TMP_Text       m_playersText;
+    [SerializeField] private TMP_Text       m_joinInfoText;
     [SerializeField] private PlayerManager  m_playerManager;
 
     private void OnEnable()
     {
         m_playerManager.OnGameStartEvent.AddListener(OnGameStart);
+        m_playerManager.OnPartyReady.AddListener(OnClientConnected);
     }
 
     private void OnDisable()
     {
         m_playerManager.OnGameStartEvent.RemoveListener(OnGameStart);
+        m_playerManager.OnPartyReady.RemoveListener(OnClientConnected);
     }
 
     private void Update()
@@ -54,9 +59,26 @@ public class Lobby : MonoBehaviour
         string IP = m_inputClientIP.text;
         int port = int.Parse(m_inputClientPort.text);
 
-        Debug.Log("Trying to join server :" + IP + "\nAt PORT :" + m_inputClientPort.text);
+        ChangeText(m_joinInfoText, "Trying to join server :" + IP + "\nAt PORT :" + m_inputClientPort.text, new Color(0.65f, 0.98f, 1.0f));
 
         //  DO join 
         m_playerManager.Join(IP, port);
+
+    }
+
+    public void OnClientUnjoin()
+    {
+        m_playerManager.DisconnectFromServer();
+    }
+
+    public void OnClientConnected()
+    {
+        ChangeText(m_joinInfoText, "Successfully connected to " + m_inputClientIP.text + ":" + m_inputClientPort.text, new Color(0.50f, 1.0f, 0.50f));
+    }
+
+    private void ChangeText(TMP_Text textUi, string text, Color color)
+    {
+        textUi.text = text;
+        textUi.color = color;
     }
 }
