ed4fff64fb12433b2b52ffa3d154e11bdfa18696
Noé Mouchel <noe.mouchel@gmail.com>
2022-09-24 19:34:39 +0200

    [spectators] kill join ongoing game feature

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,7 +25,6 @@ public class Player : MonoBehaviour
             if (host.Stream == null)
             {
                 ChessGameMgr.Instance.EnableAI(true);
-                host.WaitPlayer();
             }
 
             ChessGameMgr.Instance.team = ChessGameMgr.EChessTeam.White;
